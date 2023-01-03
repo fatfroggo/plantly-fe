a70ea333a54ee11b1de01be55893be85702dc071
JJW-27 <josephjonwilliams@outlook.com>
2023-01-03 14:49:05 +0000

    fixes featured plant

--- components/UserArea/UserArea.jsx
+++ components/UserArea/UserArea.jsx
@@ -7,40 +7,39 @@ import {
   Image,
   Pressable,
   ScrollView,
-} from "react-native";
-import Nav from "../Nav";
-import UserAreaHeader from "./UserAreaHeader";
-import Notifications from "./Notifications";
-import { useEffect, useState, useContext } from "react";
-import { getPlants, getUserPlants } from "../../api/api";
+} from 'react-native';
+import Nav from '../Nav';
+import UserAreaHeader from './UserAreaHeader';
+import Notifications from './Notifications';
+import { useEffect, useState, useContext } from 'react';
+import { getPlants, getUserPlants } from '../../api/api';
 
-import randomPlantGenerator from "./RandomPlantGenerator";
-import UserContext from "../context/userContext";
-import UserPlantsContext from "../context/userPlantsContext";
+import UserContext from '../context/userContext';
+import UserPlantsContext from '../context/userPlantsContext';
 
 const UserArea = ({ navigation }) => {
-  const [randomId, setRandomId] = useState();
   const [pressed, setPressed] = useState(false);
+  const [featuredPlant, setFeaturedPlant] = useState({});
   const { userPlantsData, setUserPlantsData } = useContext(UserPlantsContext);
   const { user, setUser } = useContext(UserContext);
 
   useEffect(() => {
-    getUserPlants(user).then((plants) => {
+    getUserPlants(user).then(plants => {
       setUserPlantsData(plants);
+    });
+  }, []);
+
+  useEffect(() => {
+    getPlants().then(res => {
+      const randomId = Math.floor(Math.random() * res.length);
+      const randomPlant = res.filter(plant => plant.plant_id === randomId);
+      setFeaturedPlant(randomPlant[0]);
     });
   }, []);
 
   const toggleIsPressed = () => {
     setPressed(true);
   };
-
-  useEffect(() => {
-    getPlants().then((res) =>
-      setRandomId(Math.floor(Math.random() * res.length))
-    );
-  }, []);
-
-  const plant = randomPlantGenerator(randomId);
 
   return (
     <View style={styles.container}>
@@ -64,23 +63,27 @@ const UserArea = ({ navigation }) => {
         <ScrollView>
           <Notifications />
           <View style={styles.featuredPlant}>
-            {typeof plant === "string" ? (
+            {typeof plant === 'string' ? (
               <Text>{plant}</Text>
             ) : (
               <>
                 <Text style={styles.featuredPlantHeader}>Featured Plant</Text>
                 <Pressable onPress={toggleIsPressed} style={styles.pressable}>
                   <View style={styles.textContainer}>
-                    <Text style={styles.commonName}>{plant.common_name}</Text>
-                    <Text style={styles.latinName}>{plant.latin_name}</Text>
-                    <View style={styles.imageContainer}>
+                    <Text style={styles.commonName}>
+                      {featuredPlant?.common_name}
+                    </Text>
+                    <Text style={styles.latinName}>
+                      {featuredPlant?.latin_name}
+                    </Text>
+                    <View style={styles?.imageContainer}>
                       <Image
-                        source={{ uri: plant.picture_url }}
-                        style={{ height: "100%", width: "100%" }}
+                        source={{ uri: featuredPlant?.picture_url }}
+                        style={{ height: '100%', width: '100%' }}
                       />
                       <Text
                         style={styles.plantInfo}
-                      >{`Climate: ${plant.climate}`}</Text>
+                      >{`Climate: ${featuredPlant?.climate}`}</Text>
                     </View>
                   </View>
                 </Pressable>
@@ -95,14 +98,14 @@ const UserArea = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#D9D9D9",
+    backgroundColor: '#D9D9D9',
     flex: 5,
   },
   safe: {
-    width: "100%",
+    width: '100%',
     flex: 0.5,
-    backgroundColor: "#2B8B30",
-    color: "#1E2720",
+    backgroundColor: '#2B8B30',
+    color: '#1E2720',
   },
   userAreaBody: {
     marginVertical: 10,
@@ -111,15 +114,15 @@ const styles = StyleSheet.create({
   },
   pressable: {
     flex: 1,
-    height: "100%",
+    height: '100%',
   },
   featuredPlant: {
-    backgroundColor: "#F1F1F2",
+    backgroundColor: '#F1F1F2',
     borderRadius: 20,
     height: 600,
-    flexDirection: "column",
+    flexDirection: 'column',
     marginTop: 10,
-    color: "black",
+    color: 'black',
     flex: 1,
     padding: 10,
   },
@@ -130,8 +133,8 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 9,
-    width: "100%",
-    height: "100%",
+    width: '100%',
+    height: '100%',
   },
   plantInfo: {
     paddingTop: 5,
@@ -143,13 +146,13 @@ const styles = StyleSheet.create({
   },
   commonName: {
     flex: 0.8,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     fontSize: 22,
   },
   latinName: {
     flex: 0.6,
     fontSize: 16,
-    fontStyle: "italic",
+    fontStyle: 'italic',
   },
 });
 
