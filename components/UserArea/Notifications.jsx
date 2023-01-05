3b8566597a7381c5acbecb3ae6e5d1d46d042ac9
abrasumente999 <katieoliviaholliday@gmail.com>
2023-01-05 16:14:27 +0000

    add some changes to dropdown

--- components/UserArea/Notifications.jsx
+++ components/UserArea/Notifications.jsx
@@ -14,6 +14,7 @@ import UserContext from "../context/userContext";
 import MyPlantModal from "./MyPlantModal";
 
 import LastWatered from "./LastWatered";
+import { dateToDays } from "../../utils/utils";
 
 const Notifications = ({
   modalLoading,
